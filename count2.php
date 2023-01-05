bc069e215b79b0ebe9a5587d53ac149b5ff51ac6
Irfanabdel <91359474+Irfanabdel@users.noreply.github.com>
2023-01-05 19:11:33 +0700

    Fix Count 2

--- count2.php
+++ count2.php
@@ -1,6 +1,6 @@
 <?php
 include "koneksi.php";
-$Q = mysqli_query($koneksi, "select count(nama_umkm) as smk FROM `umkm`") or die(mysqli_error());
+$Q = mysqli_query($koneksi, "select count(nama_umkm) as  FROM `umkm`") or die(mysqli_error());
 if ($Q) {
         $posts = array();
         if (mysqli_num_rows($Q)) {
